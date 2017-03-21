3f43e2dc50547ca7405b6f36d1bf45c7d14a1d10
Rupesh <bharuka.rupesh@gmail.com>
2017-03-21 14:50:43 +0530

    Agile Items POJO modified

--- InventroyManagement/src/com/agile/bl/model/AgileItems.java
+++ InventroyManagement/src/com/agile/bl/model/AgileItems.java
@@ -8,10 +8,24 @@ import java.sql.Timestamp;
  */
 public class AgileItems {
 	
+	private int itemId;
 	private String itemName;
 	private int quantity;
 	private String description;
 	private Timestamp lastModifiedDate;
+	
+	
+	/**
+	 *  Initializes a newly created AgileItem objects with the parameters passed
+	 *  to this method.  
+	 */
+	public AgileItems(){
+		this.itemId = 0;
+		this.itemName = "";
+		this.quantity = 0;
+		this.description = "";
+		this.lastModifiedDate = null;
+	}
 	
 	/**
 	 * Initializes a newly created AgileItem objects with the parameters passed
@@ -33,17 +47,47 @@ public class AgileItems {
 		this.description = description;
 	}
 	
-	public AgileItems(String itemName, int quantity, String description, Timestamp lastModifiedDate){
+	/**
+	 * 
+	 * 
+	 * Initializes a newly created AgileItem objects with the parameters passed
+	 * to this method.
+	 * 
+	 * @param itemId
+	 *			  An int value used to set the item id 
+	 * @param itemName
+	 *            A string value used to set the name of item
+	 * @param quantity
+	 *            An int value used to set the quantity of item
+	 * @param description
+	 * 			  A string value used to set the description of item
+	 * @param lastModifiedDate
+	 *            A Timestamp used to set the last modified date of item
+	 * 
+	 */
+	public AgileItems(int itemId, String itemName, int quantity, String description, Timestamp lastModifiedDate){
+		this.itemId = itemId;
 		this.itemName = itemName;
 		this.quantity = quantity;
 		this.description = description;
 		this.lastModifiedDate = lastModifiedDate;
 	}
 
-	public AgileItems(){
-		
-	}
 	
+	/**
+	 * @return An int which is item id
+	 */
+	public int getItemId() {
+		return itemId;
+	}
+
+	/**
+	 * @param itemId An int passed to set the item id
+	 */
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
 	/**
 	 * @return The string which is item name
 	*/
@@ -101,7 +145,7 @@ public class AgileItems {
 	}
 	
 	public String toString(){
-		return "Item Details [" +"Item Name = " +this.itemName +", Quantity = " +this.quantity
+		return "Item Details [" +"Item Id : " +itemId +"Item Name = " +this.itemName +", Quantity = " +this.quantity
 				+", Description = " +this.description +", Last Modified Date = " +this.lastModifiedDate +"] \n";
 	}
 		
