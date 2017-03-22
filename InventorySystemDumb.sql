453fc38ec1ea0c7eefba1a2806ff39e4aa1c2bb2
Rupesh <bharuka.rupesh@gmail.com>
2017-03-22 12:34:41 +0530

    InventorySystemDumb database file modified

---
+++ InventorySystemDumb.sql
@@ -0,0 +1,119 @@
+CREATE DATABASE  IF NOT EXISTS `InventorySystem` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `InventorySystem`;
+-- MySQL dump 10.13  Distrib 5.5.50, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: InventorySystem
+-- ------------------------------------------------------
+-- Server version	5.5.50-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Items`
+--
+
+DROP TABLE IF EXISTS `Items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Items` (
+  `itemid` int(11) NOT NULL AUTO_INCREMENT,
+  `itemname` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `lastmodifieddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`itemid`),
+  UNIQUE KEY `itemname_UNIQUE` (`itemname`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Items`
+--
+
+LOCK TABLES `Items` WRITE;
+/*!40000 ALTER TABLE `Items` DISABLE KEYS */;
+INSERT INTO `Items` VALUES (1,'Gel Pensss',504,'Green Gelsss','2017-03-21 11:58:41'),(2,'Red Pencil',44,'Red pencil for marking purpose','2017-03-22 06:06:26'),(3,'Water Bottle',29,'Milton Water Bottle','2017-03-21 05:13:01'),(4,'Calendar',39,'2017 Calendar','2017-03-21 11:59:56');
+/*!40000 ALTER TABLE `Items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Request`
+--
+
+DROP TABLE IF EXISTS `Request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Request` (
+  `requestid` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `itemid` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `requeststatus` tinyint(4) NOT NULL DEFAULT '2',
+  `requestdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`requestid`),
+  KEY `User_userid_idx` (`userid`),
+  KEY `Items_itemid_idx` (`itemid`),
+  CONSTRAINT `Items_itemid` FOREIGN KEY (`itemid`) REFERENCES `Items` (`itemid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `User_userid` FOREIGN KEY (`userid`) REFERENCES `User` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Request`
+--
+
+LOCK TABLES `Request` WRITE;
+/*!40000 ALTER TABLE `Request` DISABLE KEYS */;
+INSERT INTO `Request` VALUES (1,1,3,1,'I need it on urgent basis',0,'2017-03-21 05:13:01'),(3,3,2,1,'I need Pencil',1,'2017-03-21 12:00:22'),(4,3,2,1,'',0,'2017-03-21 12:00:12'),(5,3,4,1,'',1,'2017-03-21 11:59:56'),(6,3,1,1,'',2,'2017-03-21 11:59:39'),(7,1,2,5,'Please give me asap',2,'2017-03-22 05:59:59');
+/*!40000 ALTER TABLE `Request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `emailid` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `isadmin` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `emailid_UNIQUE` (`emailid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,'Rupesh','Bharuka','rupeshb@agsft.com','rupesh',1),(3,'Nilesh','Waikar','nilesh@agsft.com','nilesh',0),(4,'Sunil','More','sunilm@agsft.com','sunil',0),(5,'Salman','Khan','salman@agsft.com','salman',0),(8,'Ashish','Kale','ashish@agsft.com','ashish',0),(10,'Rupesh','Bharuka','rupeshbh@agsft.com','rupesh',0);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-22 12:32:01
